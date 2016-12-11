4ea2e82e6e872c28d749001733cedca7cf44180a
delucis <swithinbank@gmail.com>
2016-12-11 17:10:04 -0500

    Adapt Express server to work alongside Webpack structure.

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const server = express();
 var http = require('http').createServer(server);
 
-server.use(express.static('public'));
+server.use('/dist', express.static('dist'));
+
+server.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
