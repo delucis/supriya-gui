5d54ac9491330a46faceec507f97d7e3d1e26ba8
delucis <swithinbank@gmail.com>
2016-12-11 22:43:12 -0500

    Create basic button component.

---
+++ src/components/sup-button.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <button type="button" name="button">{{ supButton.name }}</button>
+</template>
+
+<script>
+export default {
+  name: 'sup-button',
+  props: [
+    'supButton'
+  ]
+}
+</script>
+
+<style lang="css">
+</style>
