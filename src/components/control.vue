31e0e9526e0ecc0ef38a73b450a524f462c61b6b
delucis <swithinbank@gmail.com>
2016-12-11 22:43:49 -0500

    Create <control> component for dynamically loading different UI controls.

---
+++ src/components/control.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <slider v-if="control.type === 'slider'" :slider="control"></slider>
+  <sup-button v-else-if="control.type === 'button'" :supButton="control"></button>
+</template>
+
+<script>
+import slider from './slider.vue'
+import supButton from './sup-button.vue'
+export default {
+  name: 'control',
+  props: [
+    'control'
+  ],
+  components: {
+    slider,
+    supButton
+  }
+}
+</script>
+
+<style lang="css">
+</style>
