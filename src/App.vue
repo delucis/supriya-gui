fc4674a647895739f3b7dca4005e73db8b68f112
delucis <swithinbank@gmail.com>
2016-12-11 22:46:49 -0500

    Create <App> component to tie everything together.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <app-bar></app-bar>
+    <supriya-controls :instruments="instruments"></supriya-controls>
+  </div>
+</template>
+
+<script>
+import appBar from './components/app-bar.vue'
+import supriyaControls from './components/supriya-controls.vue'
+import appData from './supriya-dummy-1.json'
+export default {
+  name: 'app',
+  components: {
+    appBar,
+    supriyaControls
+  },
+  props: 'controls',
+  data () {
+    return appData
+  }
+}
+</script>
