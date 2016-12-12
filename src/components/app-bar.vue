478b05197786b2c7f7ce8371bb59d4c4fac179e4
delucis <swithinbank@gmail.com>
2016-12-11 22:46:19 -0500

    Create <app-bar> component.

---
+++ src/components/app-bar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header id="app-header">
+    Supriya GUI
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app-bar'
+}
+</script>
+
+<style>
+#app-header {
+  background: lightgrey;
+  line-height: 2;
+}
+</style>
