6fa8b5d4e00fa75310cd4ea69e680aeb32a01509
delucis <swithinbank@gmail.com>
2016-12-22 23:20:20 +0100

    Add <vu-meters> and <server-status> to main App.vue view.

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
   <div id="app">
     <app-bar></app-bar>
     <supriya-controls :instruments="instruments"></supriya-controls>
+    <div style="display:flex">
+      <vu-meters v-for="(values, key) in server_meters" :values="values" :name="key"></vu-meters>
+    </div>
+    <server-status :status="status"></server-status>
   </div>
 </template>
 
 <script>
 import appBar from './components/app-bar.vue'
 import supriyaControls from './components/supriya-controls.vue'
+import vuMeters from './components/vu-meters.vue'
+import serverStatus from './components/server-status.vue'
 import appData from './supriya-dummy-1.json'
 export default {
   name: 'app',
   components: {
     appBar,
-    supriyaControls
+    supriyaControls,
+    vuMeters,
+    serverStatus
   },
   props: 'controls',
   data () {
