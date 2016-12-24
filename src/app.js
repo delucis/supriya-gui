fc556b93c0e4d986dcccaa2c1dd2193cc8c530fd
delucis <swithinbank@gmail.com>
2016-12-24 15:02:36 +0100

    Import Vuex store in app.js and include in Vue instance.

--- src/app.js
+++ src/app.js
@@ -7,9 +7,12 @@ import Vue from 'vue'
 // import app components/structure from <App> Vue.js component
 import App from './App.vue'
 
+// import Vuex store
+import store from './vuex/store.js'
 
 // bind Vue instance to div#app and render it as <App>
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
