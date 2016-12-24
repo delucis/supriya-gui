15ccdb2a3536b0f95d3c01510727d5c5ce8c149b
delucis <swithinbank@gmail.com>
2016-12-24 14:58:59 +0100

    Improve app.js commenting.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // import styling to normalize defaults across browsers
 require('normalize.css');
 
+// import and set-up Vue
 import Vue from 'vue'
 import App from './App.vue'
 
 
+// bind Vue instance to div#app and render it as <App>
 new Vue({
   el: '#app',
   render: h => h(App)
