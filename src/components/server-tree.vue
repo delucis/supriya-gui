78760b2a06e5af59c57d0df05c079efcaadad4c4
delucis <swithinbank@gmail.com>
2016-12-28 21:25:59 +0100

    Create <server-tree> component.

---
+++ src/components/server-tree.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="server-tree">
+    <server-node v-for="node in server_tree.tree.child_nodes" :node="node"></server-node>
+  </div>
+</template>
+
+<script>
+import serverNode from './server-node.vue'
+export default {
+  name: 'server-tree',
+  props: {
+    server_tree: {
+      type: Object
+    }
+  },
+  components: {
+    serverNode
+  }
+}
+</script>
+
+<style lang="css">
+.server-tree {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+}
+</style>
