fdac76bd0923fc78352e2c5d9cecea617d5688c9
delucis <swithinbank@gmail.com>
2016-12-29 00:20:17 +0100

    Write data generators to randomly update page state, mimicking server input.

--- src/app.js
+++ src/app.js
@@ -30,3 +30,40 @@ store.commit('update_server_meters', appData.server_meters)
 for (var i = 0; i < appData.server_tree.length; i++) {
   store.commit('add_server_tree_node', appData.server_tree[i])
 }
+
+// import utilities for generating fake data
+import fakers from './utils/fakers.js'
+
+// periodically update server_status in store with new fake data
+setInterval(function(){
+  let nuAvgCPU = fakers.wanderInRange(store.state.server_status.average_cpu_usage, {max: 100, maxStep: 0.2})
+  let peakCPU = store.state.server_status.peak_cpu_usage
+  let nuStatus = {
+    average_cpu_usage: nuAvgCPU,
+    peak_cpu_usage: nuAvgCPU > peakCPU ? nuAvgCPU : peakCPU
+  }
+  store.commit('update_server_status', nuStatus)
+}, 100)
+setInterval(function(){
+  let nuStatus = {
+    ugen_count: fakers.wanderInRange(store.state.server_status.ugen_count, {max: 10000, maxStep: 16, int: true}),
+    synth_count: fakers.wanderInRange(store.state.server_status.synth_count, {max: 100, maxStep: 2, int: true}),
+    group_count: fakers.wanderInRange(store.state.server_status.group_count, {max: 30, maxStep: 1, int: true})
+  }
+  store.commit('update_server_status', nuStatus)
+}, 3256)
+
+setInterval(function(){
+  for (var meters in store.state.server_meters) {
+    if (store.state.server_meters.hasOwnProperty(meters)) {
+      let newLevels = store.state.server_meters[meters]
+      for (var i = 0; i < 2; i++) {
+        newLevels[i] = fakers.fauxdio()
+      }
+      let newMeters = {
+        [meters]: newLevels
+      }
+      store.commit('update_server_meters', newMeters)
+    }
+  }
+}, 1000)
