a41cfb4a6aa96e543638f615afc8e8e9c7249ff4
delucis <swithinbank@gmail.com>
2016-12-28 21:25:05 +0100

    Completely rewrite add_server_tree_node() mutation in Vuex store module.

--- src/vuex/modules/store_server_tree.js
+++ src/vuex/modules/store_server_tree.js
@@ -1,51 +1,15 @@
-/**
-   # NOTES ON BUILDING A TREE
-
-       Nodes each have:
-       - a node_id
-       - a parent
-       - children (perhaps)
-
-       Nodes are stored in:
-       - nodes OR
-       - orphans
-
-       The full tree is stored in:
-       - tree
-
-   ## Adding a node to the tree:
-
-   Can this node be added?
-   1. Test it has a node_id & a parent property (even if null)
-   2. Test a node with that id does not already exist in nodes or orphans
-
-   Great! Should we include orphaned children while we’re at it?
-   3. Are there any node.children?
-   4. If so, are they waiting in orphans?
-   5. If so, embed them explicitly in node & remove from orphans.
-
-   OK. Where should we add the node?
-   6. Is node.parent in nodes?
-      YES:
-         If so, add to array to build breadcrumbs
-         Loop a-b) recursively using until we reach a null parent, e.g.
-                        nodes[node.parent].parent
-                  nodes[nodes[node.parent].parent].parent
-                                  etc.
-        Use breadcrumbs to add node to tree.
-      NO:
-        Add node to orphans
-
-*/
-
 import Vue from 'vue'
+import getBreadcrumbs from './../helpers/getBreadcrumbs.js'
+import getTarget from './../helpers/getTarget.js'
 
 // define a Vuex store module to handle server tree state
 export default {
   state: {
     nodes: {},
     orphans: {},
-    tree: {}
+    tree: {
+      child_nodes: {}
+    }
   },
   mutations: {
     /**
@@ -57,30 +21,71 @@ export default {
      * @see update_server_tree_node
      */
     add_server_tree_node (state, payload) {
-      if (payload.hasOwnProperty('node_id')) {
-        // build an “address” array for the node
-        // an address gives you the path to that node from the root,
-        // e.g. [0, 1, 1002] implies this has an address of nodes.0.1.1002.this
-        if(!state.nodes.hasOwnProperty(payload.node_id)) {
-          let address
-          if (payload.parent !== null) {
-            address = []
-            let id = payload.parent
-            address.unshift(id)
-            while (state.nodes[id].hasOwnProperty('parent') && state.nodes[id].parent !== null) {
-              address.unshift(state.nodes[id].parent)
-              id = state.nodes[id].parent
-            }
-          } else {
-            address = null
-          }
-          payload.address = address
-          Vue.set(state.nodes, payload.node_id, payload)
-        } else {
-          console.error('add_server_tree_node(): server_tree already contains a node with id of “' + payload.node_id + '”. Use update_server_tree_node() instead.')
-        }
-      } else {
+      // alias payload for more reasonable code
+      let node = payload
+      // make sure all conditions are met to add node to tree
+      // Make sure node has a node_id property
+      if (!node.hasOwnProperty('node_id')) {
         console.error('add_server_tree_node(): payload object must have node_id property.')
+        return
+      }
+      // Make sure node has a parent property (even if it’s null)
+      if (!node.hasOwnProperty('parent')) {
+        console.error('add_server_tree_node(): payload object must have parent property.')
+        return
+      }
+      // Make sure node’s node_id is new, i.e. not already present in nodes or orphans
+      if (state.nodes.hasOwnProperty(node.node_id)
+          || state.orphans.hasOwnProperty(node.node_id))
+      {
+        console.error('add_server_tree_node(): server_tree already contains a node with id of “' + node.node_id + '”. Use update_server_tree_node() instead.')
+        return
+      }
+      // Make sure node’s parent is in the nodes list, otherwise it is an orphan
+      // unless it’s parent is null, in which case it is a root node
+      if (node.parent !== null
+          && !state.nodes.hasOwnProperty(node.parent))
+      {
+        Vue.set(state.orphans, node.node_id, node)
+        return
+      }
+
+      // Build up a “breadcrumbs” array for tracing a node’s position in the tree
+      let breadcrumbs = getBreadcrumbs(node, state.nodes)
+      // attach “breadcrumbs” array to the node for later reference
+      Vue.set(node, 'breadcrumbs', breadcrumbs)
+
+      // Trace breadcrumbs up tree to find place to attach new node
+      let target = getTarget(state.tree, breadcrumbs)
+
+      if (!target.child_nodes) {
+        Vue.set(target, 'child_nodes', {})
+      }
+      // attach to tree and add node to nodes
+      Vue.set(target.child_nodes, node.node_id, node)
+      Vue.set(state.nodes, node.node_id, node)
+
+      // run through orphans to pick up any that have this node as parent
+      for (var orphan in state.orphans) {
+        if (state.orphans.hasOwnProperty(orphan)) {
+          if (state.orphans[orphan].hasOwnProperty('parent')
+              && state.orphans[orphan].parent === node.node_id)
+          {
+            // if orphan has the new node as a parent,
+            // add it to the tree and move it from orphans to nodes
+            let orphanNode = state.orphans[orphan]
+            let orphanBreadcrumbs = getBreadcrumbs(orphanNode, state.nodes)
+            Vue.set(orphanNode, 'breadcrumbs', orphanBreadcrumbs)
+            let orphanTarget = target.child_nodes[node.node_id]
+            if (!orphanTarget.child_nodes) {
+              Vue.set(orphanTarget, 'child_nodes', {})
+            }
+            // attach to tree, add to nodes, remove from orphans
+            Vue.set(orphanTarget.child_nodes, orphanNode.node_id, orphanNode)
+            Vue.set(state.nodes, orphanNode.node_id, orphanNode)
+            Vue.delete(state.orphans, orphanNode.node_id)
+          }
+        }
       }
     },
     /**
@@ -112,21 +117,6 @@ export default {
       } else {
         console.error('add_server_tree_node(): payload object must have node_id property.')
       }
-    }
-  },
-  getters: {
-    /**
-     * Returns an alternative server tree representation nesting nodes within their parents.
-     *
-     * @constructor
-     * @param {object} state - current state in store
-     */
-    nested_server_tree (state) {
-      let nestedServerTree = {}
-      return nestedServerTree
-    },
-    build_nested_server_tree (state) {
-
     }
   }
 }
