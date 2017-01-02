8852fe1e5d1bbe7771d071d3a3aee1b1761a719e
delucis <swithinbank@gmail.com>
2017-01-03 00:08:37 +0100

    Set up barebones socket.io server.

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const port = process.env.PORT || 8888;
 const express = require('express');
 const server = express();
 var http = require('http').createServer(server);
+var io = require('socket.io').listen(http);
 
 server.use('/dist', express.static('dist'));
 
 server.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('New socket connected.');
+  socket.on('disconnect', function () {
+    console.log('A socket disconnnected.');
+  });
 });
 
 http.listen(port, function(){
