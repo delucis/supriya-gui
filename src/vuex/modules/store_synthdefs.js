2dec95c080f77154e5b728204c3f7d321528fb44
delucis <swithinbank@gmail.com>
2017-01-03 14:08:20 +0100

    Create Vuex store module for synthdefs.

---
+++ src/vuex/modules/store_synthdefs.js
@@ -0,0 +1,69 @@
+import Vue from 'vue'
+
+export default  {
+  state: {},
+  mutations: {
+    /**
+     * Add a new synthdef to the store, and index it by its unique hash.
+     *
+     * @param {object} state - current state in store
+     * @param {object} payload - a synthdef object to add to the store
+     * @param {string} payload.hash - unique identifying hash
+     * @param {string} payload.name - human-readable name
+     * @param {object} payload.parameters - object containing definitions of the synthdef’s parameters
+     */
+    POST_SYNTHDEF (state, payload) {
+      let synthdef = payload
+      if (!synthdef.hasOwnProperty('hash')) {
+        console.error('POST_SYNTHDEF(): synthdef payload does not contain a hash property.')
+        return { status: 400 }
+      }
+      if (state.hasOwnProperty(synthdef.hash)) {
+        console.error('POST_SYNTHDEF(): a synthdef with this hash already exists in the store.')
+        return { status: 409 }
+      }
+      Vue.set(state, synthdef.hash, synthdef)
+      return { status: 201, data: { url: '/synthdefs/' + synthdef.hash } }
+    },
+    /**
+     * Update a synthdef in the store indexed by its unique hash.
+     *
+     * @param {object} state - current state in store
+     * @param {object} payload - a synthdef object to update in the store
+     * @param {string} payload.hash - unique identifying hash
+     * @param {string} payload.name - human-readable name
+     * @param {object} payload.parameters - object containing definitions of the synthdef’s parameters
+     */
+    PUT_SYNTHDEF (state, payload) {
+      let synthdef = payload
+      if (!synthdef.hasOwnProperty('hash')) {
+        console.error('PUT_SYNTHDEF(): synthdef payload does not contain a hash property.')
+        return { status: 404 }
+      }
+      if (!state.hasOwnProperty(synthdef.hash)) {
+        console.error('PUT_SYNTHDEF(): a synthdef with this hash wasn’t found.')
+        return { status: 404 }
+      }
+      Vue.set(state, synthdef.hash, synthdef)
+      return { status: 200, data: { url: '/synthdefs/' + synthdef.hash } }
+    },
+    /**
+     * Delete a synthdef in the store by its hash index.
+     *
+     * @param {object} state - current state in store
+     * @param {string} payload - a synthdef hash
+     */
+    DELETE_SYNTHDEF (state, payload) {
+      if (typeof payload !== 'string') {
+        console.error('DELETE_SYNTHDEF(): payload must be a string.')
+        return { status: 404 }
+      }
+      if (!state.hasOwnProperty(payload)) {
+        console.error('DELETE_SYNTHDEF(): a synthdef with this hash does not exist.')
+        return { status: 404 }
+      }
+      Vue.delete(state[payload])
+      return { status: 200, data: null }
+    }
+  }
+}
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -6,12 +6,14 @@ Vue.use(Vuex)
 import store_server_status from './modules/store_server_status.js'
 import store_server_meters from './modules/store_server_meters.js'
 import store_server_tree   from './modules/store_server_tree.js'
+import store_synthdefs     from './modules/store_synthdefs.js'
 
 // create Vuex store using appData
 export default new Vuex.Store({
   modules: {
     server_status: store_server_status,
     server_meters: store_server_meters,
-    server_tree: store_server_tree
+    server_tree: store_server_tree,
+    synthdefs: store_synthdefs
   }
 })
