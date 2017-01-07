5645a23095b59ba3ace3c2ebd038aa559e8cb21c
delucis <swithinbank@gmail.com>
2017-01-07 21:25:20 +0100

    Add collapse/expand all nodes buttons to <app-bar>.

--- src/components/app-bar.vue
+++ src/components/app-bar.vue
@@ -1,12 +1,42 @@
 <template>
   <header id="app-header">
-    Supriya GUI
+    <h1 class="app-header__title">
+      Supriya GUI
+    </h1>
+    <div class="app-header__controls">
+      <button @click="showAll" :disabled="isAllNodesShown">
+        Expand All Nodes
+      </button>
+      <button @click="hideAll" :disabled="isNoNodesShown">
+        Collapse All Nodes
+      </button>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'app-bar'
+  name: 'app-bar',
+  computed: {
+    isAllNodesShown () {
+      return this.$store.getters.isAllNodesShown
+    },
+    isNoNodesShown () {
+      return this.$store.getters.isNoNodesShown
+    }
+  },
+  methods: {
+    showAll () {
+      this.$store.dispatch('show_nodes', {
+        show: true
+      })
+    },
+    hideAll () {
+      this.$store.dispatch('show_nodes', {
+        show: false
+      })
+    }
+  }
 }
 </script>
 
@@ -16,7 +46,12 @@ export default {
   padding: $spacing;
   margin-bottom: $spacing;
   background: $oc-gray-1;
-  @include text-subheading;
+  display: flex;
+  justify-content: space-between;
+  .app-header__title {
+    @include text-subheading;
+    margin: 0;
+  }
   @include module-shadow;
 }
 </style>
