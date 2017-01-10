1dbff91c612194785759b1cb39768ba4701f82e8
delucis <swithinbank@gmail.com>
2017-01-10 15:54:30 +0100

    Document PATCH_SEVER_STATUS mutation.

--- src/vuex/modules/store_server_status.js
+++ src/vuex/modules/store_server_status.js
@@ -13,6 +13,20 @@ export default {
     ugen_count: 0
   },
   mutations: {
+    /**
+     * Update some or all of the server status values.
+     *
+     * @param {Object} state - current state in store
+     * @param {Object} payload - object containing key-value pairs of status values to update
+     * @param {Number} [payload.actual_sample_rate] - The actual sample rate the server is currently running at.
+     * @param {Number} [payload.average_cpu_usage] - The average CPU usage of the server.
+     * @param {Number} [payload.group_count] - The number of groups in the server’s network.
+     * @param {Number} [payload.peak_cpu_usage] - The highest CPU usage of the server.
+     * @param {Number} [payload.synth_count] - The number of synths in the server’s network.
+     * @param {Number} [payload.synthdef_count] - The number of synthdefs defined on the server.
+     * @param {Number} [payload.target_sample_rate] - The sample rate the server is aiming to run at.
+     * @param {Number} [payload.ugen_count] - The number of ugens (unit generators) in the server’s network.
+     */
     PATCH_SERVER_STATUS (state, payload) {
       for (var property in payload) {
         if (payload.hasOwnProperty(property)
