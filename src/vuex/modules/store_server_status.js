c9a4edd995aadf13d71a56abedc2ebac3f98395f
delucis <swithinbank@gmail.com>
2017-01-10 16:08:47 +0100

    Document patch_server_status action.

--- src/vuex/modules/store_server_status.js
+++ src/vuex/modules/store_server_status.js
@@ -41,6 +41,13 @@ export default {
     }
   },
   actions: {
+    /**
+     * Commits a {@link PATCH_SERVER_STATUS} mutation.
+     *
+     * @param {Object} $0 - context passed by the Vuex.dispatch() method.
+     * @param {Object} $0.commit - the Vuex.commit() method
+     * @param {Object} payload - object containing key-value pairs of status values to update, see {@link PATCH_SERVER_STATUS}
+     */
     patch_server_status ({commit}, payload) {
       commit('PATCH_SERVER_STATUS', payload)
     }
